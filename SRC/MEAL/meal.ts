d51602f2ca33f5ac54ccee5f5beeee31228cf1ba
Ngor <NG.MAM@PNC.LAN>
2024-04-26 10:32:40 +0700

    meal

---
+++ SRC/MEAL/meal.ts
@@ -0,0 +1,7 @@
+export class Meal {
+    constructor(private name: string, private position: number, private salary: number) {
+        this.name = name;
+        this.position = position;
+        this.salary = salary;
+    }
+}
