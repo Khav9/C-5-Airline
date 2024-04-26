2e09877a80d121351e6f46cc4401f7757e20aa27
Ngor <NG.MAM@PNC.LAN>
2024-04-26 10:48:39 +0700

    Pilot

--- SRC/PERSON/employee.ts
+++ SRC/PERSON/employee.ts
@@ -2,7 +2,7 @@ import { Gender } from "./gender";
 import { Person } from "./person";
 
 export abstract class Employee extends Person{
-      constructor(id:number, firstName:string, lastName:string, age: number, gender:Gender, private salary: number) {
+      constructor(id:number, firstName:string, lastName:string, age: number, gender:Gender, protected salary: number) {
             super(id,firstName,lastName,age,gender);
             this.salary = salary;
       }
---
+++ SRC/PERSON/pilot.ts
@@ -0,0 +1,12 @@
+import { Employee } from "./employee";
+import { Gender } from "./gender";
+
+export class Pilot extends Employee {
+    constructor(id: number, firstName: string, lastName: string, age: number, gender: Gender, salary: number, private license: string) {
+        super(id, firstName, lastName, age, gender, salary);
+        this.license = license;
+    }
+    public getSalary(): number {
+        return this.salary;
+    }
+}
