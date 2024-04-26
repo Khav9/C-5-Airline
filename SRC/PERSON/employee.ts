9a8a8aba9a5c13982812bcd204766ea64bf043db
khav9 <khav.saroeun@student.passerellesnumeriques.org>
2024-04-26 10:29:57 +0700

    person, employe and gender

---
+++ SRC/PERSON/employee.ts
@@ -0,0 +1,13 @@
+import { Gender } from "./gender";
+import { Person } from "./person";
+
+export abstract class Employee extends Person{
+      constructor(id:number, firstName:string, lastName:string, age: number, gender:Gender, private salary: number) {
+            super(id,firstName,lastName,age,gender);
+            this.salary = salary;
+      }
+
+      public getSalary(){
+            return this.salary;
+      }
+}
